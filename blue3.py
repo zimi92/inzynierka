95b34b5f95aca632affbe7d86ea843ce5e6724a8
Your Name <you@example.com>
2015-09-29 04:19:56 +0000

    second commit

--- blue3.py
+++ blue3.py
@@ -1,52 +1,70 @@
 import bluetooth
 import threading
 import socket
+import serial
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)	
 sock.bind(("",14))
 port = sock.getsockname()[1]
 sock.listen(1)
 data = ""
+data1 = ""
 cl_sock, cl_inf = sock.accept()
 class receiverBT(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 	def run(self):
 		try:
-			data ="" 
+			global data 
 			while data != "close":
-				data=""
+				
 				data=cl_sock.recv(1024)
 				print data
 			print "koniec "
 		except(IOError):
 			print "recvc not work"
-		print "end"
+		print "endRecvBt"
 	
 class senderBt(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 	def run(self):
 		try:
-			sock.connect(("E0:63:E5:29:49:91",15))
+			print "trying to establishe connectiion"
+			sock.connect(("E0:63:E5:29:49:91",14))
+			print "success"
 			while True:
-				data=raw_input()
-				if len(data) == 0:
+				data1=raw_input()
+				if len(data1) == 0:
 					break
-				sock.send(data)
-				print data
+				sock.send(data1)
+				print data1
 			print "koniec "
 		except(IOError):
 			print "send func not work"
-		print "end"
-class sender():
+		print "endSendBT"
+class senderSer(threading.Thread):
         def __init__(self):
-                self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.clientSocket = None
-                self.clientAddress = None
-                self.connected = False
-                self.address = ''
-
+                threading.Thread.__init__(self)
+                self.dataLocal = ""
+                try:
+                        if(False):
+                                ports = list(serial.tool.list_ports.comports())
+                                ser = serial.Serial(port = '/dev/ttyUSB0')
+                                if ser.isOpen() == False:
+                                        ser.open()
+                
+                except(IOError):
+                        print "Serial sender doesn't work"
+        def run(self):
+                global data
+                dataLocal = ""
+                while( True):
+                        if data != dataLocal:
+                                print data
+                        dataLocal = data                       
 recivBt = receiverBT()
 recivBt.start()
 sendBt = senderBt()
 sendBt.start()
+sendSer = senderSer()
+sendSer.start()
--- serialTry.py
+++ serialTry.py
@@ -2,13 +2,14 @@ import serial.tools.list_ports
 import time
 
 ports = list(serial.tools.list_ports.comports())
-sed = serial.Serial(0)
+ser = serial.Serial(port = '/dev/ttyUSB0')
 if ser.isOpen() == False:
 	ser.open()
 
 ser.write('>03050000 39 00[CR]')
 time.sleep(1)
-while ser.inWaiting() > 0:
+out = ''
+while True:
 	out += ser.read(1)
 	if out != '':
 		print '>> ' + out
